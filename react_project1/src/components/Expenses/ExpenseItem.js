f3f8441adfaa216b385459f8df14592d8edb83d4
anirudh ramaswamy <laidbackanonymous@gmail.com>
2023-03-14 14:38:09 +0530

    adding usestates

--- react_project1/src/components/Expenses/ExpenseItem.js
+++ react_project1/src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,28 @@
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
+import React, { useState } from "react";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("changed titile");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">
           {String.fromCharCode(0x20b9)} {props.amount}
         </div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
