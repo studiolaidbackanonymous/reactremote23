6261efa5351ee91da2008a5821661f3534e151a1
anirudh ramaswamy <laidbackanonymous@gmail.com>
2023-03-16 17:39:36 +0530

    adding ExpensesChart.js comp

--- react_project1/src/components/Expenses/Expenses.js
+++ react_project1/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -16,15 +17,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  console.log("expenses.js");
-  console.log(filteredExpenses);
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         onSelectedYear={filterChangeHandler}
         selected={filteredYear}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList filteredItems={filteredExpenses} />
     </Card>
   );
