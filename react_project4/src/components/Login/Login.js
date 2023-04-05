2bfe2bffda0d4736135a2c9a26cc7e31b729d50f
anirudh ramaswamy <laidbackanonymous@gmail.com>
2023-04-05 16:04:20 +0530

    adding cleanup func

--- react_project4/src/components/Login/Login.js
+++ react_project4/src/components/Login/Login.js
@@ -12,10 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
-  }, [enteredEmail, enteredPassword]);
+    const identifier = setTimeout(() => {
+      console.log("validation");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    // cleanup
+    return () => {
+      console.log("cleaning");
+      clearTimeout(identifier);
+    };
+  }, [setFormIsValid, enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
