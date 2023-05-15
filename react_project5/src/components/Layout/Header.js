5a4ca261fc618a3e64513152f2c25e8c12fbcde8
anirudh ramaswamy <laidbackanonymous@gmail.com>
2023-05-15 11:09:44 +0530

    adding header comp

--- react_project5/src/components/Layout/Header.js
+++ react_project5/src/components/Layout/Header.js
@@ -1,16 +1,16 @@
 import { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <Fragment>
-      <header>
+      <header className={classes.header}>
         <h1>ReactMeals</h1>
         <button>Cart</button>
       </header>
-      <div>
-        {" "}
-        <img src={mealsImage} />>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="a table full of delicious food" />
       </div>
     </Fragment>
   );
